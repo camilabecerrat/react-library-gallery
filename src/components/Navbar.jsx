9f641b236bf1d29db1f9905e4b8f572052c94277
camilabecerrat <camibecerra2107@icloud.com>
2025-09-22 19:42:02 -0500

    Crear el componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#222', color: '#fff' }}>
+      <h1>Biblioteca Yuval Noah Harari</h1>
+    </nav>
+  );
+}

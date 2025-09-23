e754077e10133ab6bf675175fbb41196e9e08b7d
camilabecerrat <camibecerra2107@icloud.com>
2025-09-22 19:42:28 -0500

    Crear Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer style={{ padding: '1rem', backgroundColor: '#222', color: '#fff', textAlign: 'center' }}>
+      <p>© 2025 Biblioteca Yuval Noah Harari. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
